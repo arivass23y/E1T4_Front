3cd860c296ef7da9190349fea3899e1d5461fae8
Mikel <mlopezc21@fpsanturtzilh.eus>
2025-11-05 13:23:34 +0100

    Primera subida de la pagina web

---
+++ Web-Orria/saioa-hasi.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="eu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saioa Hasi</title>
+    <link rel="stylesheet" href="estilos/saioa-hasi.css">
+</head>
+<body>
+    <img class="nav-img" src="img/Logo vertical.png" alt="Usuario">
+
+    <div id="div-hasi">
+        <form method="post" action="">
+            <label for="Erabiltzailea">Erabiltzailea</label><br>
+            <input type="text" id="izena" name="izena"><br>
+
+            <label for="pasahitza">Pasahitza</label><br>
+            <input type="password" id="pasahitza" name="pasahitza">
+            <button id="saioa-botoia" type="submit">Saioa Hasi</button>
+        </form>
+    </div>
+
+</body>
+</html>
