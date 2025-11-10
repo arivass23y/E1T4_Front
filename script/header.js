d4cab7636929f4a5688168de5ac05ab99df2618c
Alejandro <arivass23@fpsanturtzilh.eus>
2025-11-10 12:50:50 +0100

    header konponketak

--- script/header.js
+++ script/header.js
@@ -1,4 +1,4 @@
-fetch('components/header-no-search.html')
+fetch('../components/header.html')
     .then(res => res.text())
     .then(html => {
         document.getElementById('header').innerHTML = html;
@@ -16,4 +16,53 @@ fetch('components/header-no-search.html')
             sidebar.classList.remove('show');
         }
     })
+
+    const page = window.location.pathname.split("/").pop();
+    const navText = document.getElementById('nav-text');
+    const searchWrapper = document.getElementById('search-wrapper');
+
+    const pagesConfig = {
+        "index.html": {
+            title: "Biltegia",
+            search: false
+        },
+        "profila.html": {
+            title: "Profila",
+            search: false
+        },
+        "ekipamenduak.html": {
+            title: "Ekipamendua",
+            search: true,
+            searchPlaceholder: "Equipamenduaren izena..."
+        },
+        "kategoriak.html": {
+            title: "Kategoriak",
+            search: true,
+            searchPlaceholder: "Kategoriaren izena..."
+        },
+        "inbentarioak.html": {
+            title: "Inbentarioa",
+            search: true,
+            searchPlaceholder: "Inbentarioren etiketa..."
+        },
+        "gelak.html": {
+            title: "Gelak",
+            search: true,
+            searchPlaceholder: "Gelaren izena..."
+        }
+    };
+
+    const config = pagesConfig[page] || { title: "Biltegia", search: true };
+
+    navText.textContent = config.title;
+
+    if (config.search) {
+        searchWrapper.style.display = "flex";
+        if (config.searchPlaceholder) {
+            document.getElementById("nav-search").placeholder = config.searchPlaceholder;
+        }
+    } else {
+        searchWrapper.style.display = "none";
+    }
+
 });
