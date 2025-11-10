048467f5c7326fa832758daaefd6b467baf1e7a8
Alejandro <arivass23@fpsanturtzilh.eus>
2025-11-10 12:14:20 +0100

    Konponenteak konponketak

---
+++ script/header.js
@@ -0,0 +1,19 @@
+fetch('components/header-no-search.html')
+    .then(res => res.text())
+    .then(html => {
+        document.getElementById('header').innerHTML = html;
+        document.getElementsByClassName('nav-text')[0].textContent = "Biltegia";
+
+    const menuToggle = document.getElementById('menu-icon');
+    const sidebar = document.getElementById('sidebar');
+
+        menuToggle.addEventListener('click', () => {
+            sidebar.classList.toggle('show');
+        });
+
+    document.addEventListener('click', (e) => {
+        if (!sidebar.contains(e.target) && !menuToggle.contains(e.target)) {
+            sidebar.classList.remove('show');
+        }
+    })
+});
